2dafce2a45dd613fd6823321a5169d26f157176c
Michael Dinerstein <md@missionmike.dev>
2024-12-01 08:42:38 +0000

    feat: update jest coverage and use ts.

---
+++ frontend/jest.config.ts
@@ -0,0 +1,34 @@
+import type { JestConfigWithTsJest } from "ts-jest";
+
+const jestConfig: JestConfigWithTsJest = {
+  // [...]
+  // Replace `ts-jest` with the preset you want to use
+  // from the above list
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  moduleDirectories: ["node_modules", __dirname],
+  moduleNameMapper: {
+    "\\.(css|less|scss)$": "identity-obj-proxy",
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      // required due to custom location of tsconfig.json configuration file
+      // https://kulshekhar.github.io/ts-jest/docs/getting-started/options/tsconfig
+      { tsconfig: "./tsconfig.jest.json" },
+    ],
+  },
+  // transformIgnorePatterns: ["node_modules/(?!(@testing-library/jest-dom)/)"],
+  collectCoverageFrom: ["src/**/*.ts", "src/**/*.tsx", "!**/node_modules/**"],
+  coverageThreshold: {
+    global: {
+      branches: 1,
+      functions: 1,
+      lines: 1,
+      statements: 1,
+    },
+  },
+};
+
+export default jestConfig;
--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,7 +1,5 @@
-import { getProviders } from "next-auth/react";
 import SignInComponent from "./SignInComponent";
 
 export default async function SignIn() {
-  const providers = await getProviders();
-  return <SignInComponent providers={providers} />;
+  return <SignInComponent />;
 }
