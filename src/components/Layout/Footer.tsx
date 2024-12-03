8ea57e9466c52b650f906c420ac3bedeea90fe53
Michael Dinerstein <md@missionmike.dev>
2024-12-03 06:40:08 +0000

    feat: use MUI sx prop.

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -2,39 +2,54 @@
 
 import React, { useContext } from "react";
 
-import { BottomNavigation } from "@mui/material";
+import { BottomNavigation, Box } from "@mui/material";
 import { Icon } from "@iconify/react";
 import Link from "next/link";
 import { ThemeAppearanceContext } from "@/context/ThemeContext";
 import { ThemeAppearanceToggle } from "./ThemeAppearanceToggle";
-import cx from "classnames";
-import styles from "./Footer.module.scss";
 
 export const Footer = () => {
   const { themeAppearance } = useContext(ThemeAppearanceContext);
 
   return (
     <BottomNavigation
-      className={styles.footer}
       component="footer"
       sx={(theme) => ({
         backgroundColor: theme.palette.background.paper,
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        width: "100vw",
+        boxShadow: "0 0 35px rgba(0, 0, 0, 0.1)",
       })}
     >
-      <div className={styles.footerContents}>
-        <div className={styles.link}>
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            gap: "0.35em",
+          }}
+        >
           <Icon
             icon="devicon:github"
             fontSize={23}
             color="light"
-            className={cx(styles.iconify, themeAppearance === "dark" ? styles.invert : "")}
+            style={{ filter: themeAppearance === "dark" ? "invert(1)" : "" }}
           />
           <Link href="https://github.com/missionmike/interactive-resume" target="_blank">
             <span>/missionmike/interactive-resume</span>
           </Link>
-        </div>{" "}
+        </Box>
         <ThemeAppearanceToggle />
-      </div>
+      </Box>
     </BottomNavigation>
   );
 };
--- src/components/Layout/ThemeAppearanceToggle.tsx
+++ src/components/Layout/ThemeAppearanceToggle.tsx
@@ -1,8 +1,7 @@
 import { Icon } from "@iconify/react";
 
-import { Switch } from "@mui/material";
+import { Box, Switch } from "@mui/material";
 import { ThemeAppearanceContext } from "@/context/ThemeContext";
-import styles from "./ThemeAppearanceToggle.module.scss";
 import { useContext } from "react";
 
 export const ThemeAppearanceToggle = () => {
@@ -13,16 +12,23 @@ export const ThemeAppearanceToggle = () => {
   };
 
   return (
-    <div className={styles.themeAppearanceToggle}>
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        height: "50px",
+        backgroundColor: "transparent",
+      }}
+    >
       <Icon icon="solar:sun-bold" />
       <Switch
         checked={themeAppearance === "dark"}
         onChange={toggleThemeAppearance}
         sx={(theme) => ({
-          backgroundColor: theme.palette.background.default,
+          backgroundColor: theme.palette.background.paper,
         })}
       />
       <Icon icon="solar:moon-bold" />
-    </div>
+    </Box>
   );
 };
