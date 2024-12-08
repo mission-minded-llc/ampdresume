6b5de8f9097912f6fd22cf1a63a96e016356b235
Michael Dinerstein <md@missionmike.dev>
2024-12-08 22:22:32 +0000

    fix: education seed.

--- prisma/seed/education.mjs
+++ prisma/seed/education.mjs
@@ -8,38 +8,27 @@ const prisma = new PrismaClient();
 async function main() {
   const testUserIds = await getTestUserIds();
 
-  const skills = await prisma.skill.findMany({
-    where: {
-      OR: [
-        { name: { contains: "HTML", mode: "insensitive" } },
-        { name: { contains: "CSS", mode: "insensitive" } },
-        { name: { contains: "JavaScript", mode: "insensitive" } },
-      ],
-    },
-  });
-
   for (const userId of testUserIds) {
-    for (const skill of skills) {
-      const existingSkillForUser = await prisma.skillForUser.findFirst({
-        where: {
-          userId,
-          skillId: skill.id,
-        },
-      });
+    const existingEducation = await prisma.education.findFirst({
+      where: {
+        userId,
+      },
+    });
 
-      if (existingSkillForUser) {
-        console.log(`Skill ${skill.name} already exists for user ${userId}`);
-        continue;
-      }
-
-      const createdSkill = await prisma.skillForUser.create({
-        data: {
-          skillId: skill.id,
-          userId: userId,
-        },
-      });
-      console.log(`Created skill ${skill.name} for user ${userId} with id: ${createdSkill.id}`);
+    if (existingEducation) {
+      console.log(`Education already exists for user ${userId}`);
+      continue;
     }
+
+    const createdEducation = await prisma.education.create({
+      data: {
+        school: "University of Southern California",
+        degree: "B.S. Computer Science",
+        dateAwarded: new Date(),
+        userId,
+      },
+    });
+    console.log(`Created education for user ${userId} with id: ${createdEducation.id}`);
   }
 }
 
