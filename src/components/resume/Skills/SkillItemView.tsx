3d945cf69e429dc9c56840c734949c47aa22a6e3
Michael Dinerstein <md@missionmike.dev>
2024-12-16 07:23:19 +0000

    feat: parse html for descriptions.

--- src/components/resume/Skills/SkillItemView.tsx
+++ src/components/resume/Skills/SkillItemView.tsx
@@ -1,6 +1,17 @@
 import { Box } from "@mui/material";
 import { SkillForUserWithSkill } from "@/graphql/getSkillsForUser";
+import parse from "html-react-parser";
 
-export const SkillItemView = ({ skill }: { skill: SkillForUserWithSkill }) => (
-  <Box>{skill.description}</Box>
-);
+export const SkillItemView = ({ skill }: { skill: SkillForUserWithSkill }) => {
+  if (!skill?.description) return null;
+
+  let nodes;
+  try {
+    nodes = parse(skill.description);
+  } catch {
+    // TODO: Handle errors with Sentry.
+    return null;
+  }
+
+  return <Box>{nodes}</Box>;
+};
