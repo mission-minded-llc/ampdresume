2988faf71c6a2e358dee8c89645089945d7f6527
Michael Dinerstein <md@missionmike.dev>
2024-12-17 22:44:28 +0000

    feat: dependent workflow for CD, extract action.

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,8 @@ provider "aws" {
 
 # Define local variables for bucket names and environments
 locals {
-  domain       = "openresume.org"
+  domain = "openresume.org"
+  # Bucket names. local, test, and www are the environments
+  # Local is for local dev, test is for staging, and www is for production.
   environments = ["local", "test", "www"]
 }
