5e1b76c28fa82f954c6c2e099698088cd1c045aa
Michael Dinerstein <md@missionmike.dev>
2024-12-20 09:03:05 +0000

    fix: app runner.

--- terraform/security.tf
+++ terraform/security.tf
@@ -28,4 +28,24 @@ resource "aws_security_group" "app_runner" {
   name        = "app-runner-security-group"
   description = "Security group for App Runner VPC Connector"
   vpc_id      = aws_vpc.main.id
+
+  # Allow outbound traffic to RDS
+  egress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.rds_sg.id]
+  }
+
+  # Allow all other outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "app-runner-sg"
+  }
 }
