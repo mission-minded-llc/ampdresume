efef97d64521d82e99558178361b604df8687217
Michael Dinerstein <md@missionmike.dev>
2024-12-28 23:52:41 +0000

    feat: add edit page title.

--- src/app/resume/edit/page.tsx
+++ src/app/resume/edit/page.tsx
@@ -1,66 +1,11 @@
-"use client";
+import { EditPage } from "./EditPage";
 
-import { Box, Container } from "@mui/material";
+export function generateMetadata() {
+  return {
+    title: "Edit Resume",
+  };
+}
 
-import { EditPageProvider } from "./components/EditContext";
-import { EditSection } from "./components/EditSection";
-import { LoadingOverlay } from "@/components/LoadingOverlay";
-import { MuiLink } from "@/components/MuiLink";
-import { ResumeProvider } from "@/components/resume/ResumeContext";
-import { SidebarLeft } from "./components/SidebarLeft";
-import { getSkillsForUser } from "@/server/skills";
-import { useQuery } from "@tanstack/react-query";
-import { useSession } from "next-auth/react";
-
-const EditPage = () => {
-  const { data: session, status } = useSession();
-
-  const { isPending, error, data } = useQuery({
-    // Only enable the query when the session is loaded and has a user ID
-    enabled: status === "authenticated" && !!session?.user?.id,
-    queryKey: ["skills", session?.user?.id],
-    queryFn: async () => {
-      if (!session?.user?.id) {
-        throw new Error("No user ID available");
-      }
-      return await getSkillsForUser({ userId: session.user.id });
-    },
-  });
-
-  if (status === "loading") return <LoadingOverlay message="Loading session..." />;
-  if (status === "unauthenticated")
-    return (
-      <Box>
-        Please <MuiLink href="/login">log in.</MuiLink>
-      </Box>
-    );
-
-  if (isPending) return <LoadingOverlay message="Loading resume data..." />;
-  if (error) return <Box>Error loading skills: {error.message}</Box>;
-
-  const sidebarWidth = 200;
-
-  return (
-    <ResumeProvider
-      skillsForUser={data?.skillsForUser ?? []}
-      companies={[]}
-      positions={[]}
-      education={[]}
-    >
-      <EditPageProvider>
-        <Container
-          sx={{
-            display: "grid",
-            height: "100%",
-            gridTemplateColumns: `${sidebarWidth}px 1fr`,
-          }}
-        >
-          <SidebarLeft width={sidebarWidth} />
-          <EditSection />
-        </Container>
-      </EditPageProvider>
-    </ResumeProvider>
-  );
-};
-
-export default EditPage;
+export default function Page() {
+  return <EditPage />;
+}
