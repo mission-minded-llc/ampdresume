d401d8bed0a6909e59a014028260f315c01322e4
Michael Dinerstein <md@missionmike.dev>
2025-01-22 08:10:38 +0000

    feat: ignore graphql in code coverage.

--- jest.config.ts
+++ jest.config.ts
@@ -23,7 +23,7 @@ const jestConfig: JestConfigWithTsJest = {
   transformIgnorePatterns: ["node_modules"],
 
   // Coverage settings.
-  collectCoverageFrom: ["src/**/*.ts", "src/**/*.tsx", "!**/node_modules/**"],
+  collectCoverageFrom: ["src/**/*.ts", "src/**/*.tsx", "!src/graphql/**", "!**/node_modules/**"],
   coverageThreshold: {
     global: {
       branches: 1,
