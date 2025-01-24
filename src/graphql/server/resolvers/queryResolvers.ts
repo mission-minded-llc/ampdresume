c1a6c2f8144bbd77418b4336bb9c4ac766496363
Michael Dinerstein <md@missionmike.dev>
2025-01-24 07:28:26 +0000

    feat: support social links in db.

--- src/graphql/server/resolvers/queryResolvers.ts
+++ src/graphql/server/resolvers/queryResolvers.ts
@@ -4,6 +4,7 @@ import { getPositions } from "./queries/getPositions";
 import { getSkills } from "./queries/getSkills";
 import { getSkillsForProject } from "./queries/getSkillsForProject";
 import { getSkillsForUser } from "./queries/getSkillsForUser";
+import { getSocials } from "./queries/getSocials";
 import { getUser } from "./queries/getUser";
 
 export const queryResolvers = {
@@ -12,6 +13,7 @@ export const queryResolvers = {
 
   // User-specific queries.
   user: getUser,
+  socials: getSocials,
   skillsForUser: getSkillsForUser,
   companies: getCompanies,
   positions: getPositions,
