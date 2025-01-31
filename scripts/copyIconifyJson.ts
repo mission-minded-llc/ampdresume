c137edcd2a43831bd184c2136abb4158aefc981f
Michael Dinerstein <md@missionmike.dev>
2025-01-31 20:02:25 +0000

    fix: exclude the largest json files.

--- scripts/copyIconifyJson.ts
+++ scripts/copyIconifyJson.ts
@@ -9,8 +9,22 @@ if (!fs.existsSync(targetDir)) {
   fs.mkdirSync(targetDir, { recursive: true });
 }
 
+// Exclude these files, they're too large.
+const excludedFiles = [
+  "fluent.json",
+  "fluent-emoji.json",
+  "fluent-emoji-flat.json",
+  "noto.json",
+  "twemoji.json",
+  "arcticons.json",
+];
+
 fs.readdirSync(sourceDir)
   .filter((file) => file.endsWith(".json"))
   .forEach((file) => {
+    if (excludedFiles.includes(file)) {
+      return;
+    }
+
     fs.copyFileSync(path.join(sourceDir, file), path.join(targetDir, file));
   });
