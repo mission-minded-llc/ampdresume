98cf0b1249bbe1bd7541ab461ac8f83cf7a525b9
Michael Dinerstein <md@missionmike.dev>
2025-03-14 02:12:18 +0000

    feat: move cypress deps to root.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "cypress";
-import filePlugin from "./plugins/file-plugin";
+import filePlugin from "./cypress/plugins/file-plugin";
 
 export default defineConfig({
   e2e: {
@@ -9,8 +9,8 @@ export default defineConfig({
     },
     baseUrl: "http://localhost:3000",
     env: {
-      BASE_URL: "http://localhost:3000",
-      TEST_EMAIL: "test@openresume.org",
+      BASE_URL: process.env.CYPRESS_BASE_URL || "http://localhost:3000",
+      TEST_EMAIL: process.env.CYPRESS_TEST_EMAIL || "test@openresume.org",
     },
     chromeWebSecurity: false, // Might be needed for navigating to magic link
     specPattern: "./**/*.cy.{js,jsx,ts,tsx}",
