fe92ae89384eb5acae6c33f3a4317648d487b84a
Michael Dinerstein <md@missionmike.dev>
2025-03-17 05:50:19 +0000

    feat: update profile tests.

--- cypress/integration/account/profile.cy.ts
+++ cypress/integration/account/profile.cy.ts
@@ -1,10 +1,47 @@
 describe("Profile Section", () => {
-  beforeEach(() => {
+  before(() => {
     cy.loginWithMagicLink();
+
+    cy.getCookie("next-auth.session-token").then((cookie) => {
+      Cypress.env("sessionToken", cookie?.value || "");
+    });
+
+    cy.getCookie("next-auth.csrf-token").then((cookie) => {
+      Cypress.env("csrfToken", cookie?.value || "");
+    });
   });
 
   it("should access protected profile section", () => {
     cy.visit(`${Cypress.env("BASE_URL") || ""}/edit/profile`);
     cy.contains("Profile").should("be.visible");
+  });
+
+  it("should populate profile data and save", () => {
+    cy.setCookie("next-auth.session-token", Cypress.env("sessionToken") || "");
+    cy.setCookie("next-auth.csrf-token", Cypress.env("csrfToken") || "");
+
+    cy.visit(`${Cypress.env("BASE_URL") || ""}/edit/profile`);
+
+    const fields = [
+      { name: "name", value: "Test User" },
+      { name: "slug", value: "test-user" },
+      { name: "displayEmail", value: "test@openresume.org" },
+      { name: "title", value: "Professional Tester" },
+      { name: "location", value: "Test City, Test State" },
+      { name: "siteTitle", value: "Test SEO Site Title" },
+      { name: "siteDescription", value: "Test SEO Site Description" },
+    ];
+
+    fields.forEach((field) => {
+      cy.get(`input[name='${field.name}']`)
+        .clear({ force: true })
+        .type(field.value, { force: true, delay: 25 });
+    });
+
+    cy.get("[data-test-id=AccountFormSaveButton]").click();
+
+    // Ensure the LoadingOverlay shows up until save is completed.
+    cy.contains("Saving...").should("be.visible");
+    cy.contains("Saving...", { timeout: 8000 }).should("not.be.visible");
   });
 });
