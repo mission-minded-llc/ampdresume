38be4497406fd9b598d501a1e0b16b048364a0f8
Michael Dinerstein <md@missionmike.dev>
2025-03-17 23:03:50 +0000

    feat: update profile tests and fix validation error.

--- cypress/integration/account/profile.cy.ts
+++ cypress/integration/account/profile.cy.ts
@@ -1,14 +1,13 @@
 describe("Profile Section", () => {
+  const testEmail = "test@openresume.org";
+  const saveButton = "[data-test-id='AccountFormSaveButton']";
+
   before(() => {
     cy.loginWithMagicLink();
+  });
 
-    cy.getCookie("next-auth.session-token").then((cookie) => {
-      Cypress.env("sessionToken", cookie?.value || "");
-    });
-
-    cy.getCookie("next-auth.csrf-token").then((cookie) => {
-      Cypress.env("csrfToken", cookie?.value || "");
-    });
+  beforeEach(() => {
+    cy.setNextAuthCookies();
   });
 
   it("should access protected profile section", () => {
@@ -17,15 +16,12 @@ describe("Profile Section", () => {
   });
 
   it("should populate profile data and save", () => {
-    cy.setCookie("next-auth.session-token", Cypress.env("sessionToken") || "");
-    cy.setCookie("next-auth.csrf-token", Cypress.env("csrfToken") || "");
-
     cy.visit(`${Cypress.env("BASE_URL") || ""}/edit/profile`);
 
     const fields = [
       { name: "name", value: " Test User" },
       { name: "slug", value: "test-user" },
-      { name: "displayEmail", value: "test@openresume.org" },
+      { name: "displayEmail", value: testEmail },
       { name: "title", value: "Professional Tester " },
       { name: "location", value: " Test City, Test State " },
       { name: "siteTitle", value: " Test SEO Site Title" },
@@ -38,7 +34,8 @@ describe("Profile Section", () => {
         .type(field.value, { force: true, delay: 25 });
     });
 
-    cy.get("[data-test-id=AccountFormSaveButton]").click();
+    const saveButton = "[data-test-id='AccountFormSaveButton']";
+    cy.get(saveButton).click();
 
     cy.wait(1000); // Give it a second to save.
     cy.reload();
@@ -47,5 +44,39 @@ describe("Profile Section", () => {
     fields.forEach((field) => {
       cy.get(`input[name='${field.name}']`).should("have.value", field.value.trim());
     });
+  });
+
+  it("should encounter slug validation error", () => {
+    const slugErrorMessage = "Slug must be alphanumeric and lowercase. Hyphens allowed.";
+
+    cy.visit(`${Cypress.env("BASE_URL") || ""}/edit/profile`);
+
+    cy.contains(slugErrorMessage).should("not.exist");
+    cy.get("input[name='slug']").clear({ force: true }).type("test user");
+    cy.get(saveButton).click();
+
+    cy.get("[id='alert-dialog-description']").contains(slugErrorMessage).should("be.visible");
+    cy.get("button").contains("OK").click();
+
+    cy.get("input[name=slug]").clear({ force: true }).type("test-user");
+    cy.contains(slugErrorMessage).should("not.exist");
+
+    cy.get(saveButton).click();
+  });
+
+  it("should encounter email validation error", () => {
+    cy.visit(`${Cypress.env("BASE_URL") || ""}/edit/profile`);
+
+    cy.contains("Invalid email address").should("not.exist");
+    cy.get("input[name='displayEmail']")
+      .clear({ force: true })
+      .type(testEmail.substring(0, testEmail.length - 4)); // Remove the ".org" suffix.
+
+    cy.get(saveButton).click();
+    cy.contains("Invalid email address").should("be.visible");
+
+    cy.get("input[name=displayEmail]").type(".org"); // Adds missing suffix.
+    cy.get(saveButton).click();
+    cy.contains("Invalid email address").should("not.exist");
   });
 });
