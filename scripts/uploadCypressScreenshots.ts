4e2732dec265db382833e7204dd0d8fd34d87d2e
Michael Dinerstein <md@missionmike.dev>
2025-03-17 07:17:44 +0000

    feat: support subfolders.

--- scripts/uploadCypressScreenshots.ts
+++ scripts/uploadCypressScreenshots.ts
@@ -3,48 +3,70 @@
  * Helper script to upload cypress screenshots to S3.
  */
 
-import { readFileSync, readdirSync } from "fs";
+import { readFileSync, readdirSync, statSync } from "fs";
 
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { fileURLToPath } from "url";
 import { getS3Client } from "../src/lib/s3";
 import { join } from "path";
 
-const s3 = getS3Client();
+function getAllFiles(dirPath: string, arrayOfFiles: string[] = []): string[] {
+  const files = readdirSync(dirPath);
 
-const bucket = "ci.openresume.org";
+  files.forEach((file) => {
+    const filePath = join(dirPath, file);
+    if (statSync(filePath).isDirectory()) {
+      getAllFiles(filePath, arrayOfFiles);
+    } else {
+      arrayOfFiles.push(filePath);
+    }
+  });
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = join(__filename, "..");
+  return arrayOfFiles;
+}
 
-console.log("Uploading screenshots to S3...");
-const screenshotsDir = join(__dirname, "..", "cypress", "screenshots");
+function uploadScreenshots() {
+  const s3 = getS3Client();
 
-console.log("Local screenshots directory:", screenshotsDir);
-const files = readdirSync(screenshotsDir);
-console.log("Screenshot files list:", files);
+  const bucket = "ci.openresume.org";
 
-const s3dir = "cypress/screenshots/" + new Date().toISOString();
-console.log("S3 bucket:", bucket);
-console.log("S3 directory:", s3dir);
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = join(__filename, "..");
 
-files.forEach(async (file) => {
-  const filePath = join(screenshotsDir, file);
-  const data = readFileSync(filePath);
+  console.log("Uploading screenshots to S3...");
+  const screenshotsDir = join(__dirname, "..", "cypress", "screenshots");
 
-  console.log("Uploading file:", file);
-  await s3
-    .send(
-      new PutObjectCommand({
-        Bucket: bucket,
-        Key: `${s3dir}/${file}`,
-        Body: data,
-        ContentType: "image/png",
-      }),
-    )
-    .catch((error) => {
-      console.error("Error uploading file:", file, error);
-    });
+  console.log("Local screenshots directory:", screenshotsDir);
 
-  console.log("Done.");
-});
+  const files = getAllFiles(screenshotsDir).map((filePath) =>
+    filePath.replace(`${screenshotsDir}/`, ""),
+  );
+  console.log("Screenshot files list:", files);
+
+  const s3dir = "cypress/screenshots/" + new Date().toISOString();
+  console.log("S3 bucket:", bucket);
+  console.log("S3 directory:", s3dir);
+
+  files.forEach(async (file) => {
+    const filePath = join(screenshotsDir, file);
+    const data = readFileSync(filePath);
+
+    console.log("Uploading file:", file);
+    await s3
+      .send(
+        new PutObjectCommand({
+          Bucket: bucket,
+          Key: `${s3dir}/${file}`,
+          Body: data,
+          ContentType: "image/png",
+        }),
+      )
+      .catch((error) => {
+        console.error("Error uploading file:", file, error);
+      });
+
+    console.log("Done.");
+  });
+}
+
+uploadScreenshots();
