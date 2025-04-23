e8eb990ded7b40bcf2567af10609497dfae07dbf
Michael Dinerstein <md@missionmike.dev>
2025-04-23 15:01:33 +0000

    fix: update selector.

--- cypress/integration/account/experience.cy.js
+++ cypress/integration/account/experience.cy.js
@@ -103,8 +103,10 @@ describe("Experience Section", () => {
     cy.get("h3").contains(companyName).click();
     cy.get("h3").contains(positionTitle).click();
     cy.contains(projectTitle).dblclick();
-    cy.get(".MuiDialog-container textarea.MuiInputBase-input").clear().type(newProjectTitle);
-    cy.get(".MuiDialog-container button").contains("Save").click();
+    cy.get(".MuiDialog-root .MuiDialog-container textarea.MuiInputBase-input")
+      .clear()
+      .type(newProjectTitle);
+    cy.get(".MuiDialog-root .MuiDialog-container button").contains("Save").click();
 
     cy.contains(newProjectTitle).should("be.visible");
   });
