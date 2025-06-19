e7fb82646e863969a786632be18d1ca2e38f9e63
Michael Dinerstein <md@missionmike.dev>
2025-06-19 22:55:14 +0000

    fix: missing import.

--- src/app/edit/components/RichTextEditor/ColorPicker.tsx
+++ src/app/edit/components/RichTextEditor/ColorPicker.tsx
@@ -1,6 +1,5 @@
-import { Box, IconButton } from "@mui/material";
+import { Box, ClickAwayListener, IconButton } from "@mui/material";
 
-import { ClickAwayListener } from "@base-ui-components/react";
 import { SketchPicker } from "react-color";
 import { useState } from "react";
 
